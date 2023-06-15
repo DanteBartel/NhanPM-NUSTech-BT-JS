31c87e7fd748199cd11e2968e2fbf2ebf0b1e85f
DanteBartel <dantedbartel@gmail.com>
2023-06-16 00:03:14 +0700

    230616 0003 final

--- script/scripting.js
+++ script/scripting.js
@@ -1,41 +1,47 @@
 // Init global variables
-var e = ["", ""]
-var nowIdx = 0;
+var ans = "";
+var justCal = true;
+var isStart = true;
 var isDot = false;
-var op = "";
-var ans = 0;
-// Function to reset the state of the calculator
-function reset() {
-    document.getElementById("screendigit").innerHTML = "0";
-    e[0] = "";
-    e[1] = "";
-    nowIdx = 0;
-    isDot = false;
-    op = "";
-    ans = "";
-}
-// Function to handle button clicks
+var isZero = false;
+// Function
 function p_b_num(n) {
-    // if (nowIdx == 0) {
-    //     e[0] += "0";
-    // }
-    document.getElementById("screendigit").innerHTML = n;
+    if (justCal) {
+        ans = n;
+        justCal = false;
+    } else {
+        if (isZero) return;
+        ans += n;
+    }
+    isStart = false;
+    document.getElementById("screendigit").innerHTML = ans;
 }
 function p_b_dot() {
-    document.getElementById("screendigit").innerHTML = ".";
+    if (isDot) return;
+    else {
+        if (justCal) {
+            ans = "0";
+            justCal = false;
+        } 
+        ans += ".";
+        isDot = true;
+    }    
+    document.getElementById("screendigit").innerHTML = ans;
 }
 function p_b_eq() {
-    document.getElementById("screendigit").innerHTML = "=";
+    try {ans = eval(ans);}
+    catch (e) {ans = "0";}
+    if (ans == undefined) ans = "0";
+    justCal = true;
+    isDot = false;
+    document.getElementById("screendigit").innerHTML = ans;
 }
-function p_b_add() {
-    document.getElementById("screendigit").innerHTML = "+";
-}
-function p_b_sub() {
-    document.getElementById("screendigit").innerHTML = "-";
-}
-function p_b_div() {
-    document.getElementById("screendigit").innerHTML = "/";
-}
-function p_b_mul() {
-    document.getElementById("screendigit").innerHTML = "*";
+function p_b_op(op) {
+    if (isStart) return;
+    else {
+        if (justCal) justCal = false;
+        ans += op;
+    }    
+    isDot = false;
+    document.getElementById("screendigit").innerHTML = ans;
 }
