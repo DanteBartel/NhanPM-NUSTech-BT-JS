8b8f8c7bdeb14ec9e9485df448f10fc5cbd14e31
DanteBartel <dantedbartel@gmail.com>
2023-06-15 16:43:05 +0700

    230615 1642 init js func

---
+++ script/scripting.js
@@ -0,0 +1,48 @@
+function p_b0() {
+    document.getElementById("screendigit").innerHTML = "0";
+}
+function p_b1() {
+    document.getElementById("screendigit").innerHTML = "1";
+}
+function p_b2() {
+    document.getElementById("screendigit").innerHTML = "2";
+}
+function p_b3() {
+    document.getElementById("screendigit").innerHTML = "3";
+}
+function p_b4() {
+    document.getElementById("screendigit").innerHTML = "4";
+}
+function p_b5() {
+    document.getElementById("screendigit").innerHTML = "5";
+}
+function p_b6() {
+    document.getElementById("screendigit").innerHTML = "6";
+}
+function p_b7() {
+    document.getElementById("screendigit").innerHTML = "7";
+}
+function p_b8() {
+    document.getElementById("screendigit").innerHTML = "8";
+}
+function p_b9() {
+    document.getElementById("screendigit").innerHTML = "9";
+}
+function p_b_dot() {
+    document.getElementById("screendigit").innerHTML = ".";
+}
+function p_b_eq() {
+    document.getElementById("screendigit").innerHTML = "=";
+}
+function p_b_add() {
+    document.getElementById("screendigit").innerHTML = "+";
+}
+function p_b_sub() {
+    document.getElementById("screendigit").innerHTML = "-";
+}
+function p_b_div() {
+    document.getElementById("screendigit").innerHTML = "*";
+}
+function p_b_mul() {
+    document.getElementById("screendigit").innerHTML = "/";
+}
